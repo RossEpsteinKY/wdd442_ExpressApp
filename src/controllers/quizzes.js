4b433adf130081bc5b81a46b1814155cd781e2c4
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-08-05 21:01:26 -0400

    setting quizzes controller implemented

--- src/controllers/quizzes.js
+++ src/controllers/quizzes.js
@@ -5,13 +5,11 @@ const {param} = require("express/lib/router");
 const bodyParser = require('body-parser');
 const app = express();
 
-const quizzesController = require('./src/controllers/quizzes');
-
-app.get('/',(req,res) =>{
-    res.send("HOMEPAGE");
+router.get('/',(req,res) =>{
+    res.send('get quizzes');
 })
 
-app.use('/quizzes', quizzesController);
 
-app.listen(5000);
-console.log('listening on port 5000');
+module.exports = router;
+
+
