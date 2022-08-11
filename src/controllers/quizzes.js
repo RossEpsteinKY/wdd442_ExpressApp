66bbd62d243a347842084bba6e74d09e1ced4c5b
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-08-10 22:14:56 -0400

    Quizzes has CRUD functionality that is persistent

--- src/controllers/quizzes.js
+++ src/controllers/quizzes.js
@@ -3,80 +3,77 @@ const router = express.Router();
 const {request, response} = require("express");
 const {param} = require("express/lib/router");
 const bodyParser = require('body-parser');
-const { Quiz } = require('../models');
+const { Quizzes } = require('../models');
 const app = express();
 let quizzes = require('../../old_models/quizzes.model');
 app.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
-router.post('/',async (req, res) => {
+router.get('/',async (req, res) => {
+    // res.send('get quizzes');
+    const quizzes = await Quizzes.findAll()
+
+    res.json(quizzes);
+})
+
+router.get('/:id',async (req, res) => {
 
     try {
-        const {name} = req.body;
-        const quiz = await Quiz.create({ name })
+        const id = req.params.id;
+        const quiz = await Quizzes.findByPk(id)
+        if (!quiz) {
+            throw new Error('QUIZ NOT FOUND');
+        }
         res.json(quiz);
+        return;
+    } catch (e) {
+        res.send("ERROR: UNABLE TO FIND QUIZ ID " + req.params.id, 404);
+    }
+})
+
+
+router.post('/',async (req, res) => {
+    console.log('body', req.body);
+    try {
+
+        const name = req.body;
+        const quizzes = await Quizzes.create(name)
+        // res.json(quiz);
         res.send('successfully created');
     } catch (e) {
         res.send("ERROR: UNABLE TO FIND QUIZ ID " + req.params.id, 404);
     }
 })
 
-router.get('/',async (req, res) => {
-    // res.send('get quizzes');
-    const quiz = await Quiz.findAll()
-    res.send("Got all quizzes");
-})
 
+router.put('/:id',async (req, res) => {
 
-
-router.get('/:id',(req,res) =>{
     try {
-        const id = req.params.id;
-        const quiz = quizzes.find(quiz => quiz.id == id )
+        const id = Number(req.params.id);
+        console.log('body', req.body);
+        const name = req.body.name;
 
-        if(!quiz){
-            throw new Error('QUIZ NOT FOUND');
-        }
-        res.json(quiz);
-    }
-    catch (e) {
-        res.send("ERROR: UNABLE TO FIND QUIZ ID " + req.params.id, 404);
-    }
-})
+        const quizToUpdate = await Quizzes.update({ name }, {
+            where: { id }
+        })
 
-
-
-
-
-router.put('/:id',(req,res) =>{
-    const id = Number(req.params.id);
-    console.log('body',req.body);
-    try {
-        quizzes.map((q) => {
-            if(id === q.id){
-                    id: Number(id),
-                    q.name = req.body.name
-                }
-            return q;
-            }
-        );
-        res.json(quizzes);
+        console.log('this',quizToUpdate)
         res.send('successfully updated quiz ' + id);
-    }
-    catch (e) {
+    } catch (e) {
         res.send("ERROR: UNABLE TO UPDATE QUIZ ID " + req.params.id, 404);
     }
 })
 
-router.delete('/:id',(req,res) =>{
+router.delete('/:id',async (req, res) => {
     const id = Number(req.params.id);
-    console.log('body',req.body);
+    console.log('id',id);
     try {
-        quizzes = quizzes.filter(q => q.id !== id);
+        await Quizzes.destroy({
+            where: { id }
+        })
         res.send('successfully deleted quiz ' + id);
-    }
-    catch (e) {
+    } catch (e) {
         res.send("ERROR: UNABLE TO DELETE QUIZ ID " + req.params.id, 404);
     }
 })
