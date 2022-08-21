cd1451398d92a1270e896382bfb0cd7a7bff1d4b
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-08-21 15:37:06 -0400

    updates and creates quizzes

--- src/controllers/quizzes.js
+++ src/controllers/quizzes.js
@@ -5,7 +5,7 @@ const {param} = require("express/lib/router");
 const bodyParser = require('body-parser');
 const { Quizzes } = require('../models');
 const app = express();
-let quizzes = require('../../old_models/quizzes.model');
+// let quizzes = require('../../old_models/quizzes.model');
 app.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
@@ -35,6 +35,21 @@ router.get('/:id',async (req, res) => {
     }
 })
 
+
+router.get('/:id/show',async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const quiz = await Quizzes.findByPk(id)
+        if (!quiz) {
+            throw new Error('QUIZ NOT FOUND');
+        }
+        res.render('quiz/show',{quiz});
+        return;
+    } catch (e) {
+        res.send("ERROR: UNABLE TO FIND QUIZ ID " + req.params.id, 404);
+    }
+})
 
 router.get('/:id/edit',async (req, res) => {
 
@@ -85,7 +100,38 @@ router.put('/:id',async (req, res) => {
     }
 })
 
+router.post('/:id/edit',async (req, res) => {
+
+    try {
+        const id = Number(req.params.id);
+        console.log('body', req.body);
+        const name = req.body.name;
+
+        const quizToUpdate = await Quizzes.update({ name }, {
+            where: { id }
+        })
+
+        console.log('this',quizToUpdate)
+        res.send('successfully updated quiz ' + id);
+    } catch (e) {
+        res.send("ERROR: UNABLE TO UPDATE QUIZ ID " + req.params.id, 404);
+    }
+})
+
 router.delete('/:id',async (req, res) => {
+    const id = Number(req.params.id);
+    console.log('id',id);
+    try {
+        await Quizzes.destroy({
+            where: { id }
+        })
+        res.send('successfully deleted quiz ' + id);
+    } catch (e) {
+        res.send("ERROR: UNABLE TO DELETE QUIZ ID " + req.params.id, 404);
+    }
+})
+
+router.delete('/:id/delete',async (req, res) => {
     const id = Number(req.params.id);
     console.log('id',id);
     try {
