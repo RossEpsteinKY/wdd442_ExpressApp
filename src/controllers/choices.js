560127ed993b608fdfd5f13eedb060e3c4fab4f6
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-08-21 17:17:24 -0400

    choices has crud

--- src/controllers/choices.js
+++ src/controllers/choices.js
@@ -18,6 +18,10 @@ router.get('/',async (req, res) => {
     res.render('choices/index',{choices});
 })
 
+router.get('/new',(req,res) => {
+    res.render('choices/create')
+})
+
 router.get('/:id/show',async (req, res) => {
 
     try {
@@ -57,9 +61,43 @@ router.post('/',async (req, res) => {
         const choice = req.body;
         const choices = await Choices.create(choice)
         // res.json(choice);
-        res.send('successfully created');
+        res.redirect(`/choices/${choices.id}/show`)
     } catch (e) {
         res.send("ERROR: UNABLE TO FIND CHOICE ID " + req.params.id, 404);
+    }
+})
+router.get('/:id/edit',async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const choice = await Choices.findByPk(id)
+        if (!choice) {
+            throw new Error('CHOICE NOT FOUND');
+        }
+        res.render('choices/edit',{choice});
+        return;
+    } catch (e) {
+        res.send("ERROR: UNABLE TO FIND QUIZ ID " + req.params.id, 404);
+    }
+})
+
+router.post('/:id/edit',async (req, res) => {
+
+    try {
+        const id = Number(req.params.id);
+        console.log('id',id);
+        console.log('body', req.body);
+        const choice = req.body.choice;
+
+        const choiceToUpdate = await Choices.update({ choice }, {
+            where: { id }
+        })
+
+        console.log('this',choice);
+        // res.send('successfully updated quiz ' + id);
+        res.redirect(`/choices/${id}/show`);
+    } catch (e) {
+        res.send("ERROR: UNABLE TO UPDATE QUIZ ID " + req.params.id, 404);
     }
 })
 
@@ -90,6 +128,19 @@ router.delete('/:id',async (req, res) => {
             where: { id }
         })
         res.send('successfully deleted choice ' + id);
+    } catch (e) {
+        res.send("ERROR: UNABLE TO DELETE CHOICE ID " + req.params.id, 404);
+    }
+})
+
+router.get('/:id/delete',async (req, res) => {
+    const id = Number(req.params.id);
+    console.log('id',id);
+    try {
+        await Choices.destroy({
+            where: { id }
+        })
+        res.redirect('/choices');
     } catch (e) {
         res.send("ERROR: UNABLE TO DELETE CHOICE ID " + req.params.id, 404);
     }
